b5f9e6123f059e8fff64f4d5e9a8a1bcafccd0bf
HWY-HWY <2551628690@qq.com>
2018-05-25 14:26:20 +0800

    talk robot

--- 练习代码/微信公众号/robot.py
+++ 练习代码/微信公众号/robot.py
@@ -3,9 +3,12 @@ import json
 # import time
 
 
+# 创建post函数
 def robot(content):
+    # 图灵api
     api = r'http://openapi.tuling123.com/openapi/api/v2'
     # userid = int(time.time())
+    # 创建post提交的数据
     data = {
         "perception": {
             "inputText": {
@@ -13,16 +16,22 @@ def robot(content):
                          }
                       },
         "userInfo": {
-                    "apiKey": "672035b1bde5440e83eaexxxxxxxx",
+                    "apiKey": "672035b1bde5440e83eaexxxxxxx",
                     "userId": '672035b1bde5440',
                     }
     }
+    # 转化为json格式
     jsondata = json.dumps(data)
+    # 发起post请求
     response = requests.post(api, data=jsondata)
+    # 将返回的json数据解码
     robot_res = json.loads(response.content)
+    # 提取对话数据
     print(robot_res["results"][0]['values']['text'])
 
 
+# 创建对话死循环
 while True:
+    # 输入对话内容
     content = input("talk:")
     robot(content)
